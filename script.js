64d9a8f8923d64c6f2eba8fda3cba332e4aac2e7
Nishita-codes <nishitapatidar03@gmail.com>
2024-12-28 18:50:11 +0530

    added files

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Welcome to spotify")
+console.log("Welcome to spotify");
+
+//initialize the variables
+let songIndex=0;
+let audioElement = new Audio('')
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar')
+let songs =[
+    {songName: "Let me Love You" , filePath:"song/" , CoverPath: "Covers/"},
+    {songName: "Let me Love You" , filePath:"song/" , CoverPath: "Covers/"},
+    {songName: "Let me Love You" , filePath:"song/" , CoverPath: "Covers/"},
+    {songName: "Let me Love You" , filePath:"song/" , CoverPath: "Covers/"},
+    {songName: "Let me Love You" , filePath:"song/" , CoverPath: "Covers/"},
+    {songName: "Let me Love You" , filePath:"song/" , CoverPath: "Covers/"},
+    {songName: "Let me Love You" , filePath:"song/" , CoverPath: "Covers/"},
+    {songName: "Let me Love You" , filePath:"song/" , CoverPath: "Covers/"},
+    {songName: "Let me Love You" , filePath:"song/" , CoverPath: "Covers/"}
+]
+
+// audioElement.play();
+// handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play()
+    }
+}) 
+// Listen to Events
+myProgressBar.addEventListener('timeupdate',()=>{
+    console.log("timeupdate")
+})
